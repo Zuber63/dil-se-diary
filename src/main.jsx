35e588be7a569526eecd23df7741bbc01281c9e1
Zuber Saifi <royalsaifi63@gmail.com>
2026-09-17 12:54:40 +0530

    fix: simplify redux store import path

--- src/main.jsx
+++ src/main.jsx
@@ -2,14 +2,13 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
-import { store } from '../src/Redux/store.js'
-import {Provider} from 'react-redux'
+import { store } from './Redux/store.js'
+import { Provider } from 'react-redux'
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
     <Provider store={store}>
-
-    <App />
+      <App />
     </Provider>
   </StrictMode>,
 )
